60aa2d07084327bd8d2228cd5ee81ddfee912874
Charles Nyabeze <charlesnnyabeze@gmail.com>
2021-12-28 04:51:22 -0500

    half done formation

--- homepage.js
+++ homepage.js
@@ -18,19 +18,15 @@ var getUserRepos = function(user) {
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
-
-    // get value from input element
-var username = nameInputEl.value.trim();
-
-if (username) {
-  //getUserRepos(username);
-  nameInputEl.value = "";
-} else {
-  alert("Please enter a GitHub username");
-}
+  // get value from input element
+  var username = nameInputEl.value.trim();
+    if (username) {
+      //getUserRepos(username);
+      nameInputEl.value = "";
+    } else {
+      alert("Please enter a GitHub username");
+    }
 
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
-
-getUserRepos("lernantino");
